d71b26a3e0fbb544956768188567d9a31a91dc5f
Piotr Ziembicki <pzmz@poczta.onet.pl>
2016-01-01 13:40:43 +0100

    Commit for testing

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "run.h"
 
 int main(char **argv, int argc) {
    printf("This is Hello World C file\n");
    printf("Hello World and Happy New Year 2016\n"); 
+   printf("Pi = %f\n", getPi()); 
 }
