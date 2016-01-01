f9cec2a87a048a2b2613ca0d300b4427723675da
Piotr Ziembicki <pzmz@poczta.onet.pl>
2016-01-01 11:11:40 +0100

    File stat.c added

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(char **argv, int argc) {
+   printf("This is Hello World C file\n");
+   printf("Hello World and Happy New Year 2016\n"); 
+}
---
+++ run.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
+double getPi() {
+    return 3.141592563;
+}
